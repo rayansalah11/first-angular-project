82effe9a2f09943495478856807537ea881324d7
RAYAN SALAH <rayan.salah11@gmail.com>
2023-06-06 15:31:51 +0300

    addcomponent

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ButtonComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ angular/src/app/components/button/button.component.ts
@@ -0,0 +1,10 @@
+import { Component ,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+
+}
